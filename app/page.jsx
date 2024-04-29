ca9201d850f0e3761b5d699b5def1c7ebb37b7c9
mirza irtiza <mirzairtiza51@gmail.com>
2024-04-29 14:19:14 +0500

    changes of structure

--- app/page.jsx
+++ app/page.jsx
@@ -1,21 +1,21 @@
 import Link from 'next/link';
-import { ContextAlert } from 'components/context-alert';
+// import { useEffect, useState } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
+
+
 
 
 
 export default function Page() {
+    // const [featureBook, setFeatureBook] = useState([]);
+    // const [bookCopy, setBookCopy] = useState([]);
+    // const [loading, setLoading] = useState(false);
     return (
         <main className="flex flex-col gap-8 sm:gap-16">
             <section className="flex flex-col items-start gap-3 sm:gap-4">
-                <ContextAlert />
+                
                 <h1 className="mb-0">Netlify Platform Starter - Next.js</h1>
-                <p className="text-lg">Get started with Next.js and Netlify in seconds.</p>
-                <Link
-                    href="https://docs.netlify.com/frameworks/next-js/overview/"
-                    className="btn btn-lg btn-primary sm:btn-wide"
-                >
-                    Read the Docs
-                </Link>
+              
             </section>
           
         </main>
